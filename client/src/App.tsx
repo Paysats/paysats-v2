0598bd77f0799f9018457b33a8eece9ef8e4386e
Imam Dahir <imamddahir@gmail.com>
2025-12-30 21:17:00 +0100

    feat: add not found page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,9 +3,13 @@ import './App.css'
 import { ConfigProvider, theme } from 'antd'
 import { Route, Routes } from 'react-router-dom'
 import { AuthProvider } from './contexts/ThemeContext';
-import {Homepage} from './pages/home';
+import { Homepage } from './pages/home';
+import { isAppSubdomain } from './utils/isAppSubdomain';
+import NotFound from './pages/NotFound';
 function App() {
   const [mounted, setMounted] = useState(false);
+
+  const isApp = isAppSubdomain();
 
   // Dark mode is default, can be toggled later with a theme context
   useEffect(() => {
@@ -21,19 +25,20 @@ function App() {
 
   return (
     <ConfigProvider
-        theme={{
-          token: {
-            colorPrimary: "#33C279",
-            // colorPrimaryText: "#1a1a1a",
-            // colorTextLightSolid: "#1a1a1a", // controls text color for primary buttons
-          },
-          algorithm: theme.defaultAlgorithm,
-        }}
-      >
-        <Routes>
-          <Route path="/" element={<Homepage />} /> 
-        </Routes>
-      </ConfigProvider>
+      theme={{
+        token: {
+          colorPrimary: "#33C279",
+          // colorPrimaryText: "#1a1a1a",
+          // colorTextLightSolid: "#1a1a1a", // controls text color for primary buttons
+        },
+        algorithm: theme.defaultAlgorithm,
+      }}
+    >
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </ConfigProvider>
   )
 }
 
